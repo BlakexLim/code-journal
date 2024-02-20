05403a6e86661d326bbe3ee59aff662462b44bff
Blake Lim <blakelim007@gmail.com>
2024-02-20 22:28:30 +0000

    Completed journal-create-entries #1

--- js/main.js
+++ js/main.js
@@ -1,43 +1,25 @@
-/* global data */
-// create an object to store input values
-interface FormElements extends HTMLFormControlsCollection {
-  title: HTMLInputElement;
-  newUrl: HTMLInputElement;
-  notes: HTMLTextAreaElement;
-  entryId?: number;
-}
-
-interface DataValues {
-  title: string;
-  newUrl: string;
-  notes: string;
-  entryId?: number;
-}
-
+'use strict';
 // query the DOM for the photo input element
 // query the DOM for the img element
 // the input value is going to listen for an event and assign the event.value to the src attribute of the img element
 const $imageLink = document.getElementById('newUrl');
 const $image = document.querySelector('img');
 // query the DOM for the form element and give it a type of HTMLFormELement
-const $form = document.querySelector('form') as HTMLFormElement;
-
+const $form = document.querySelector('form');
 if (!$imageLink) throw new Error('$getImage query failed');
 if (!$image) throw new Error('$image query failed');
 if (!$form) throw new Error('$form query failed');
-
 // add event listener to the variable assigned with the photo-link input
 // assign event.target to a variable and use type assertion as HTMLInputElement
 // reassign src attribute of the img tag to value of eventTarget
-$imageLink.addEventListener('input', (event: Event) => {
-  const eventTarget = event.target as HTMLInputElement;
+$imageLink.addEventListener('input', (event) => {
+  const eventTarget = event.target;
   $image.src = eventTarget.value;
 });
-
-$form.addEventListener('submit', (event: Event) => {
+$form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const $formElements = $form.elements as FormElements;
-  const formData: DataValues = {
+  const $formElements = $form.elements;
+  const formData = {
     title: $formElements.title.value,
     newUrl: $formElements.newUrl.value,
     notes: $formElements.notes.value,
